1212c1836b61d549275a0be0e3f6c411117b36b3
github-1-star <64357531+github-1-star@users.noreply.github.com>
2022-05-31 18:01:13 +0530

    Add files via upload

---
+++ hii.php
@@ -0,0 +1,31 @@
+<?php
+
+$method=$_SERVER["REQUEST_METHOD"];
+if($method=="POST"){
+	$requestBody=file_get_contents('php://input');
+	$json=$json_decode($requestBody);
+	$text=$json->result->parameters->text;
+	switch($text){
+	case "hii":
+		$speech="hii,nice to meet you";
+		break;
+	case "bye":
+			$speech="bye,good night";
+			break;
+	case "anything":
+				$speech="yes,you can type anything here";
+				break;
+	default:
+			$speech="sorry not getting clearly you can ask other";
+					break;
+	}
+	$response=new \stdClass();
+	$response->speech="";
+	$response->displayText="";
+	$response->source="webhook";
+	echo json_encode($response);
+}
+else{
+	echo "method not allowed";
+}
+?>
